ef6a72ca68ce4c6db1f61c0f8e626ad61d4438a6
Nilton Heck <nilton@conbuss.com>
2023-04-21 23:01:46 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+function addGPTButton() {
+  const chatBubbles = document.querySelectorAll("li[id*=chat-messages]");
+  console.log(chatBubbles);
+
+  for (let i = 0; i < chatBubbles.length; i++) {
+    const chatBubble = chatBubbles[i];
+    const content = chatBubble.querySelector("div[id*=message-content]")
+      .textContent;
+
+    const el = document.createElement("button");
+    el.innerText = "Hei!";
+    el.addEventListener("click", function (e) {
+      // e.target.parentElement;
+      console.log(`Message content: ${content}`);
+
+      const gptResponse = `Blablabla at ${new Date()}`;
+
+      let findGptChatBubble = chatBubble.querySelector(
+        `div[id=gpt-response-${chatBubble.id}]`
+      );
+
+      if (!findGptChatBubble) {
+        findGptChatBubble = document.createElement("div");
+        findGptChatBubble.id = `gpt-response-${chatBubble.id}`;
+      }
+
+      findGptChatBubble.innerText = gptResponse;
+      findGptChatBubble.style.border = "1px solid red";
+
+      chatBubble.appendChild(findGptChatBubble);
+    });
+
+    chatBubble.appendChild(el);
+  }
+}
+
+// setTimeout(addGPTButton, 500);
+
+addGPTButton();
